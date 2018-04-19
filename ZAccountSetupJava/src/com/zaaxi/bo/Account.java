2deaf2bd76277a9f5460bf792710f7de8f7c822c
inr-at-089036016488 <inr@192.168.0.14>
2018-04-19 18:03:15 -0400

    Test comments for Mathew learning

--- ZAccountSetupJava/src/com/zaaxi/bo/Account.java
+++ ZAccountSetupJava/src/com/zaaxi/bo/Account.java
@@ -1,15 +1,25 @@
 package com.zaaxi.bo;
 
 public class Account {
+
+	private String accountNumber;
 	
-	private String accountNumber; 
+	private String accountName;
 
 	public String getAccountNumber() {
 		return accountNumber;
 	}
 
 	public void setAccountNumber(String accountNumber) {
-		this.accountNumber = accountNumber;  
-	}		 
+		this.accountNumber = accountNumber;
+	}
 
-} 
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+}
